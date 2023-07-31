36b86a7cad5042a15cb71ce72e2cf345b375978c
Ahmad Asadi <ahmad.asadi00@gmail.com>
2023-07-31 13:50:22 +0330

    add preprocess functions

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
-# football-odds-analysis
-Football Betting Odds Analysis and Bookmaker Margins Exploration
+# Football Betting Odds Analysis and Bookmaker Margins Exploration
+
+Football Betting Odds Analysis and Bookmakers Margins Exploration
+This repository contains data related to football betting odds and bookmaker margins
+
+Data Source
+The data for this analysis can be found on Kaggle. You can access it through the following link:
+
+Link to Dataset: https://www.kaggle.com/datasets/ahmadasadi00/football-betting-odds
+
+Data Description
+The dataset used in this repository includes historical betting odds and bookmaker margin information from five major bookmakers. The data spans a wide range of European football leagues and covers the period from 2005 to mid-2023. It comprises detailed information on various betting markets, such as match outcomes, betting odds.
+
+The dataset is structured in a well-organized format, making it easy to explore, analyze, and draw insights from the data. It provides a comprehensive view of how bookmakers have set their odds over the years, offering valuable information for both sports enthusiasts and data analysts.
+
+Repository Structure
+This repository is structured into the following sections:
+
+notebooks/: This directory contains Jupyter notebooks that demonstrate the analysis and exploration of the football betting odds and bookmaker margins data. The notebooks are named appropriately to indicate their contents.
+
+visualizations/: This directory includes interactive visualizations and graphs generated from the data. These visualizations aid in understanding odds movements, bookmaker profitability, and potential arbitrage opportunities.
 
 Key Features:
 
@@ -14,3 +33,11 @@ Arbitrage Betting Analysis: The repository investigates the concept of arbitrage
 Interactive Visualizations: To facilitate a deeper understanding of the data, the repository includes interactive visualizations and graphs. These visual representations offer intuitive insights into odds movements, bookmaker profitability, and arbitrage possibilities, enabling users to make informed decisions based on data-driven analyses.
 
 Whether you are a sports enthusiast, a data scientist, or a betting enthusiast, this repository provides a valuable resource for understanding football betting odds' intricacies and the strategies major bookmakers employ. Explore the data, identify trends, and uncover potential arbitrage opportunities to gain a competitive edge in sports betting.
+
+Note
+Please note that while the data is sourced from Kaggle, it is essential to review and comply with any licensing or usage restrictions specified by Kaggle or the dataset's creators. Always attribute the data appropriately and follow any terms of use specified by the data source.
+
+Contributing
+If you wish to contribute to this repository, whether by providing additional data, submitting bug fixes, or enhancing the analysis, feel free to create pull requests or open issues.
+
+Happy analyzing and betting exploration!
---
+++ preprocess.py
@@ -0,0 +1,110 @@
+import pandas as pd
+import warnings
+import pickle
+import typing as T
+warnings.filterwarnings('ignore')
+
+# companies
+# Bet365
+# Bet&Win
+# Interwetten
+# William Hill
+# VC Bet
+
+def cal_prob(df: pd.DataFrame, 
+             companycode: str, 
+             prefix: str = '') -> pd.DataFrame:
+    """calculates the probability of each outcome based on betting odds
+
+    Args:
+        df (pd.DataFrame): dataframe of games should have 3 columns of 
+        `companycode`H (HomeTeam Winnig Odds), `companycode`D (Draw Odds) and `companycode`A (AwayTeam Winning Odds)
+        companycode (str): betting companycode i.e. for Bet&Win is bw
+        prefix (str, optional): prefix for column name of output Defaults to ''.
+
+    Returns:
+        pd.DataFrame: the odds input dataframe that now include probabality of each outcome based on odds i.e. 1/odd
+    """
+    df[prefix + 'ProbH'] = 1/df[f'{companycode}H']
+    df[prefix + 'ProbA'] = 1/df[f'{companycode}A']
+    df[prefix + 'ProbD'] = 1/df[f'{companycode}D']
+    return df
+
+
+def calc_bookmakers_margin(df: pd.DataFrame, 
+                           companycode: str,
+                           prefix: str = '') -> pd.DataFrame:
+    """adds a column to betting odds dataframe as the bookmaker's margin
+
+    Args:
+        df (pd.DataFrame): betting odds dataframe
+        companycode (str): companycode
+        prefix (str, optional): column prefix Defaults to ''.
+
+    Returns:
+        pd.DataFrame: betting odds dataframe
+    """
+    
+    df[prefix + 'return_on_game'] = (1 / df[f'{companycode}H']) + (1 / df[f'{companycode}D']) + (1 / df[f'{companycode}A']) - 1
+    return df
+
+
+def preprocess_pipeline(odds_data_address: str) -> T.Dict:
+    """gets address of betting odds csv file address and preprocess the data, 
+       raw data source https://www.football-data.co.uk/data.php
+    Args:
+        odds_data_address (str): address to betting odds csv file for download the data please refer to https://www.kaggle.com/datasets/ahmadasadi00/football-betting-odds
+
+    Returns:
+        T.Dict:  dictionary which keys are the bookmaker's names and contains each bookmakers odds and probabilities and margins
+    """
+    data = pd.read_csv(odds_data_address, index_col=0)
+    main_cols = ['Div', 'Date', 'HomeTeam' ,'AwayTeam', 'FTR']
+    avg_cols = ['AvgH', 'AvgD', 'AvgA','AvgProbH','AvgProbA','AvgProbD', 'Avg_return_on_game']
+    avg_df = data[main_cols]
+
+    cols_b365 = ['Div','Date', 'HomeTeam' ,'AwayTeam', 'FTR', 'B365H', 'B365D', 'B365A']
+    cols_bw = ['Div','Date', 'HomeTeam' ,'AwayTeam', 'FTR', 'BWH', 'BWD', 'BWA']
+    cols_iw = ['Div','Date', 'HomeTeam' ,'AwayTeam', 'FTR', 'IWH', 'IWD', 'IWA']
+    cols_wh = ['Div','Date', 'HomeTeam' ,'AwayTeam', 'FTR', 'WHH', 'WHD', 'WHA']
+    cols_vc = ['Div','Date', 'HomeTeam' ,'AwayTeam', 'FTR', 'VCH', 'VCD', 'VCA']
+
+    b365_df = data[cols_b365]
+    bw_df = data[cols_bw]
+    iw_df = data[cols_iw]
+    wh_df = data[cols_wh]
+    vc_df = data[cols_vc]
+
+    avg_df['AvgH'] = data[['BWH','IWH','WHH','VCH']].mean(axis=1)
+    avg_df['AvgD'] = data[['BWD','IWD','WHD','VCD']].mean(axis=1)
+    avg_df['AvgA'] = data[['BWA','IWA','WHA','VCA']].mean(axis=1)
+
+    b365_df = cal_prob(b365_df, 'B365')
+    bw_df = cal_prob(bw_df, 'BW')
+    iw_df = cal_prob(iw_df, 'IW')
+    wh_df = cal_prob(wh_df, 'WH')
+    vc_df = cal_prob(vc_df, 'VC')
+    avg_df = cal_prob(avg_df, 'Avg', 'Avg')
+
+    b365_df = calc_bookmakers_margin(b365_df, 'B365')
+    bw_df = calc_bookmakers_margin(bw_df, 'BW')
+    iw_df = calc_bookmakers_margin(iw_df, 'IW')
+    wh_df = calc_bookmakers_margin(wh_df, 'WH')
+    vc_df = calc_bookmakers_margin(vc_df, 'VC')
+    avg_df = calc_bookmakers_margin(avg_df, 'Avg', 'Avg_')
+
+    b365_df = pd.concat([b365_df, avg_df[avg_cols]], axis=1)
+    bw_df = pd.concat([bw_df, avg_df[avg_cols]], axis=1)
+    iw_df = pd.concat([iw_df, avg_df[avg_cols]], axis=1)
+    wh_df = pd.concat([wh_df, avg_df[avg_cols]], axis=1)
+    vc_df = pd.concat([vc_df, avg_df[avg_cols]], axis=1)
+    avg_df = pd.concat([avg_df, avg_df[avg_cols]], axis=1)
+
+
+    betting_odds_clean_data = {'Bet365': b365_df,
+                               'Bet&Win': bw_df,
+                               'Interwetten': iw_df,
+                               'William_Hill': wh_df,
+                               'VC_Bet': vc_df,}
+    
+    return betting_odds_clean_data
